1b56d4b0683ed0bfb2f926d380aa0f038ff41103
Fabio <fabiobanachi@gmail.com>
2025-12-19 16:38:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-<script src="/main.js" defer></script>
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Elisa OS iniciado");
+
+  const buttons = document.querySelectorAll(".tab-bar button");
+  const mainView = document.querySelector(".main-view");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const section = button.innerText;
+
+      mainView.innerHTML = `
+        <h1>${section}</h1>
+        <p>Seção ${section} carregada.</p>
+
+        <div id="modules">
+          Conteúdo em desenvolvimento.
+        </div>
+      `;
+    });
+  });
+});
