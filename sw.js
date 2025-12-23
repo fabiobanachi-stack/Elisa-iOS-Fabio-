7e0771b59b714187696d0572f188b41218012475
Fabio <fabiobanachi@gmail.com>
2025-12-22 21:16:27 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,27 @@
+const CACHE_NAME = "elisa-cache-v1";
+const urlsToCache = [
+  "/index.html",
+  "/styles.css",
+  "/main.js",
+  "/app.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  console.log("Service Worker ativo");
+  event.waitUntil(self.clients.claim());
+  console.log("Service Worker ativo e pronto para offline");
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
