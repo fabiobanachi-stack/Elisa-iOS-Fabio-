d7813b3e6759bd8c1e788ab2cfb33af6601a5c76
Fabio <fabiobanachi@gmail.com>
2025-12-22 21:15:55 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-// app.js
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Elisa OS iniciado");
-
-  // Render inicial
   ElisaOS.render("home");
 
-  // Botões da Tab Bar
   const buttons = document.querySelectorAll(".tab-bar button");
-
   buttons.forEach(button => {
     button.addEventListener("click", () => {
       const view = button.dataset.view;
       if (!view) return;
-
       ElisaOS.render(view);
     });
   });
